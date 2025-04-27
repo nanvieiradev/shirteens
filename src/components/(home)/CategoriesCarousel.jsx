a6ab855156c651d4fbff8969708783baa4637421
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2025-04-27 08:31:24 -0300

    Update CategoriesCarousel.jsx

--- src/components/(home)/CategoriesCarousel.jsx
+++ src/components/(home)/CategoriesCarousel.jsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card/Index"
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel/Index"
 
 export function CategoriesCarousel() {
   return (
